981a8807e29f665c464e1923e97570088638fd1b
Davi Job <davi.job@hotmail.com>
2023-11-16 18:02:53 -0300

    Hero video and necessary configurations
    Changed hero img to a video for more details
    
    Added necessary css changes to base styling and queries
    
    Made a function to await the video load to show page and realize some actions

--- src/scripts/index-script.js
+++ src/scripts/index-script.js
@@ -16,3 +16,17 @@ document
     .addEventListener("click", function () {
         scrollToJustAbove(document.querySelector(".projects"), 60);
     });
+
+// If .hero-video has loaded and is playing, add class to hidden to body-cover and set html overflow-y to auto
+
+let heroVideo = document.querySelector(".hero-video");
+let bodyCover = document.querySelector(".body-cover");
+const techStackListItems = document.querySelectorAll(".tech-stack-list-item");
+
+heroVideo.addEventListener("play", function () {
+    bodyCover.classList.add("hidden");
+    document.documentElement.style.overflowY = "auto";
+    techStackListItems.forEach((item) => {
+        item.classList.add("animated");
+    });
+});
